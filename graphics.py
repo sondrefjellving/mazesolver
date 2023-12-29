a70700abcaec5673be0db54d2949af801cbe6524
sondrefjellving <sondrefjellving@gmail.com>
2023-12-29 18:40:02 +0100

    add cell class that makes cells based on coordinates

--- graphics.py
+++ graphics.py
@@ -5,7 +5,7 @@ class Window:
         self.__root = Tk()
         self.__root.title("Maze solver")
         self.__root.geometry(f"{w}x{h}")
-        self.__canvas = Canvas(self.__root)
+        self.__canvas = Canvas(self.__root, height=h, width=w)
         self.__canvas.pack()
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -42,3 +42,33 @@ class Line:
             self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
             )
         canvas.pack()
+
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        self.__win = win
+        self.has_top_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+        self.has_left_wall = True
+
+    def draw(self):
+        if self.has_top_wall:
+            wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(wall, "black")
+
+        if self.has_right_wall:
+            wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(wall, "black")
+
+        if self.has_bottom_wall:
+            wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(wall, "black")
+
+        if self.has_left_wall:
+            wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(wall, "black")
+
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 win = Window(800, 600)
-start_point = Point(50, 50)
-end_point = Point(200, 200)
-line = Line(start_point, end_point)
+cell = Cell(5, 5, 55, 55, win)
+cell2 = Cell(55, 5, 105, 55, win)
+cell3 = Cell(105, 5, 600, 55, win)
+cell.has_right_wall = False
+cell2.has_left_wall = False
+cell2.has_bottom_wall = False
+cell.draw()
+cell2.draw()
+cell3.draw()
 
-win.draw_line(line, "red")
 win.wait_for_close()
