30bc414d583658411dcb6a7686ef3ca126ed6c47
sondrefjellving <sondrefjellving@gmail.com>
2023-12-30 08:17:45 +0100

    add animate function

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from graphics import Window, Point, Line, Cell
 from maze import Maze
 
 win = Window(800, 600)
-maze = Maze(5, 5, 10, 15, 50, 50, win)
-
+maze = Maze(5, 5, 6, 9, 80, 80, win)
 win.wait_for_close()
--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from graphics import Cell
+import time
 
 class Maze:
     def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
@@ -13,12 +14,12 @@ class Maze:
         self.__create_cells()
 
     def __create_cells(self):
-        for c in range(0, self.num_cols):
+        for c in range(self.num_cols):
             col = []
-            for r in range(0, self.num_rows):
+            for r in range(self.num_rows):
                 col.append(Cell(self.win))
             self.__cells.append(col)
-            
+
             for r in range(self.num_rows):
                 self.__draw_cell(c, r)
 
@@ -29,5 +30,11 @@ class Maze:
         x2 = x1 + self.cell_size_x
         y2 = y1 + self.cell_size_y
         self.__cells[c][r].draw(x1, y1, x2, y2)
+        self.__animate()
+
+    def __animate(self):
+        self.win.redraw()
+        time.sleep(0.01)
+
 
 
